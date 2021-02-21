c7d06609027704ef976bb46969eddc426a16801c
rze <noemail@esa.int>
2021-02-22 00:13:43 +0100

    removed comments

--- v5_pico_start/20210221a.py
+++ v5_pico_start/20210221a.py
@@ -2,8 +2,6 @@ import time
 import rp2
 from machine import Pin
 
-# Define the blink program.  It has one GPIO to bind to on the set instruction, which is an output pin.
-# Use lots of delays to make the blinking visible by eye.
 @rp2.asm_pio(sideset_init=(rp2.PIO.OUT_LOW, rp2.PIO.OUT_LOW))
 def blink():
     wrap_target()
@@ -21,13 +19,9 @@ def blink():
     wrap()
    
 
-# Instantiate a state machine with the blink program, at 1000Hz, with set bound to Pin(25) (LED on the rp2 board)
-
 # The frequency (which must be between 2000 and 125000000)
 sm = rp2.StateMachine(0, blink, freq=48_310, sideset_base=Pin(1,0))
-#sm = rp2.StateMachine(0, blink, freq=1250000, sideset_base=Pin(1,0))
-#sm = rp2.StateMachine(0, blink, sideset_base=Pin(0,1,2))
-# Run the state machine for 3 seconds.  The LED should blink.
+
 sm.active(1)
 time.sleep(10)
 sm.active(0)
