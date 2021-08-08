e3bb0a6391d35a24ce730f2d425e8183c3440bfd
rze <noemail@esa.int>
2021-08-08 21:03:11 +0200

    works inside main

--- v8_prototype_version_1/simulator.py
+++ v8_prototype_version_1/simulator.py
@@ -1,8 +1,11 @@
 from machine import Pin, SPI
 from time import sleep, sleep_ms
-#from mcp3008 import MCP3008
-from rp2 import PIO, StateMachine, asm_pio
-#from machine import Pin, mem32
+
+# initialize
+# todo: start and stop state machines
+
+
+
 
 class MCP3008:
 
@@ -47,106 +50,142 @@ class MCP3008:
         self.cs.value(1) # turn off
         return ((self._in_buf[1] & 0x03) << 8) | self._in_buf[2]
 
-@rp2.asm_pio(sideset_init=PIO.OUT_LOW)
-def pwm_prog():
-    #pull(noblock) .side(0)
-    pull(noblock)
-    mov(x, osr) # Keep most recent pull data stashed in X, for recycling by noblock
-    mov(y, isr) # ISR must be preloaded with PWM count max
-    jmp(not_x, "continue")
-    #jmp("continue")
-    #label("null")
-    nop().side(0)
-    label("continue")
-    label("pwmloop")
-    jmp(x_not_y, "skip")
-    nop()         .side(1)
-    label("skip")
-    jmp(y_dec, "pwmloop")
+def main():
+    
 
-class PIOPWM:
-    def __init__(self, sm_id, pin, count_freq, max_count=(1 << 16) - 1):
-        print(pin)
-        self._sm = StateMachine(sm_id, pwm_prog, freq=2 * count_freq, sideset_base=Pin(pin))
-        # Use exec() to load max count into ISR
-        self._sm.put(max_count)
-        self._sm.exec("pull()")
-        self._sm.exec("mov(isr, osr)")
-        self._sm.active(1)
-        self._max_count = max_count
-        self._pin = pin
+    print(type(mcp3008_spi))
+    print(type(mcp3008_cs))
+    print(type(mcp3008_out_buf))
+    print(type(mcp3008_in_buf))
+    
+    mcp3008_cs.value(0) # select
+    print(mcp3008_cs.value())
+    mcp3008_out_buf[1] = ((not False) << 7) | (0 << 4)
+    mcp3008_spi.write_readinto(mcp3008_out_buf,mcp3008_in_buf)
+    mcp3008_cs.value(1) # turn off
+    print(mcp3008_cs.value())
+    print( ((mcp3008_in_buf[1] & 0x03) << 8) | mcp3008_in_buf[2])
+    #print("read(0) {}".format(read(0))
+    #print("Before: b={}".format(b))
+    
 
-    def _set(self, value):
-        # Minimum value is -1 (completely turn off), 0 actually still produces narrow pulse
-        if value>self._max_count:
-            self._sm.active(0)
-            print("off: ", value)
-            Pin(self._pin, Pin.OUT).value(1)
-        else:
-            if not(self._sm.active()):
-                self._sm.put(self._max_count)
-                self._sm.exec("pull()")
-                self._sm.exec("mov(isr, osr)")
-                self._sm.active(1)
-                print("on: ", value)
-            value = max(value, -1)
-            value = min(value, self._max_count)
-            self._sm.put(value)
-        
-    def set(self, value):
-        # Minimum value is -1 (completely turn off), 0 actually still produces narrow pulse
-        value = max(value, -1)
-        value = min(value, self._max_count)
-        print(value)
-        self._sm.put(value)
+def initialize():
+    '''this is the doc string of the function'''
+    global mcp3008_spi
+    global mcp3008_cs
+    global mcp3008_out_buf
+    global mcp3008_in_buf
+    
+    mcp3008_spi = SPI(0, sck=Pin(2),mosi=Pin(3),miso=Pin(4), baudrate=10000)
+    mcp3008_cs = Pin(5, Pin.OUT)
+    mcp3008_cs.value(1) # disable chip at start
+    mcp3008_out_buf = bytearray(3)
+    mcp3008_out_buf[0] = 0x01
+    mcp3008_in_buf = bytearray(3) 
 
-spi = SPI(0, sck=Pin(2),mosi=Pin(3),miso=Pin(4), baudrate=10000)
-cs = Pin(5, Pin.OUT)
-cs.value(1) # disable chip at start
-
-led = Pin(25, Pin.OUT)
-led.off()
-
-chip = MCP3008(spi, cs)
-
-state = True
-
-a=[chip.read(7),chip.read(6),chip.read(5),chip.read(4),chip.read(0),chip.read(1),chip.read(2),chip.read(3)]
-l=a
-print([l[0]//16,l[1]//16,l[2]//16,l[3]//16,l[4]//16,l[5]//16,l[6]//16,l[7]//16])
-
-pwm1=PIOPWM(1, 14, 65_79)
-#pwm2=PIOPWM(2, 15, 65_7900)
-
-#PIOPWM(2, 15, pwm_prog, max_count=(1 << 16) - 1, count_freq=int(657_900))
-
-pwm1.set(int((1<<16)*(l[2]//16)/64))
-#pwm2.set(int((1<<16)*0.75))
-
-#sleep(5)
-
-#pwm1.set(-1)
-#pwm2.set(-1)
+def read1(pin):
+    '''this is the doc string of the function'''
+    global mcp3008_spi
+    global mcp3008_cs
+    global mcp3008_out_buf
+    global mcp3008_in_buf
+    
+    print(mcp3008_cs.value())
+    
+    mcp3008_cs.value(0) # select
+    print(mcp3008_cs.value())
+    mcp3008_out_buf[1] = ((not False) << 7) | (pin << 4)
+    mcp3008_spi.write_readinto(mcp3008_out_buf,mcp3008_in_buf)
+    mcp3008_cs.value(1) # turn off
+    return ((mcp3008_in_buf[1] & 0x03) << 8) | mcp3008_in_buf[2]
 
 
-#sm4.active(0)
+if __name__ == '__main__':
+   
+    print("start")
+    
+    spi = SPI(0, sck=Pin(2),mosi=Pin(3),miso=Pin(4), baudrate=10000)
+    cs = Pin(5, Pin.OUT)
+    cs.value(1) # disable chip at start
 
-while True:
+    #chip = MCP3008(spi, cs)
+
+    cs.value(1) # ncs on
+    out_buf = bytearray(3)
+    out_buf[0] = 0x01
+    in_buf = bytearray(3)
+    
+    #chip.read(7)
+    
+    is_differential = False
+    pin = 7
+    cs.value(0) # select
+    out_buf[1] = ((not is_differential) << 7) | (pin << 4)
+    spi.write_readinto(out_buf, in_buf)
+    cs.value(1) # turn off
+    #return ((self._in_buf[1] & 0x03) << 8) | self._in_buf[2]
+    a = ((in_buf[1] & 0x03) << 8) | in_buf[2]
+    print(a)
+    print("ende")
+   
+#     mcp3008_spi = SPI(0, sck=Pin(2),mosi=Pin(3),miso=Pin(4), baudrate=10000)
+#     mcp3008_cs = Pin(5, Pin.OUT)
+#     mcp3008_cs.value(1) # disable chip at start
+#     mcp3008_out_buf = bytearray(3)
+#     mcp3008_out_buf[0] = 0x01
+#     mcp3008_in_buf = bytearray(3) 
+# 
+#     mcp3008_cs.value(0) # select
+#     sleep(0.5)
+#     pin = 0
+#     is_differential = False
+#     
+#     mcp3008_out_buf[1] = ((not is_differential) << 7) | (pin << 4)
+#     mcp3008_spi.write_readinto(mcp3008_out_buf, mcp3008_in_buf)
+#     mcp3008_cs.value(1) # turn off
+#     b = ((mcp3008_in_buf[1] & 0x03) << 8) | mcp3008_in_buf[2]
+#     print(b)
+    #main()
+
+
+
+#led = Pin(25, Pin.OUT)
+#led.off()
+
+#chip = MCP3008(spi, cs)
+
+#state = True
+
+#l=[0,0,0,0,0,0,0,0]
+
+#while True:
+    #in_0.value(state)
+    #in_1.value(not state)
+    #sleep(0.05)
+    #state = not state
+    #actual_0 = chip.read(0)
+    #actual_1 = chip.read(1)
+    #actual_2 = chip.read(2)
+    #actual_3 = chip.read(3)
+    #actual_4 = chip.read(4)
+    #actual_5 = chip.read(5)
+    #actual_6 = chip.read(6)
+    #actual_7 = chip.read(7)
+    #print(actual_0, end='')
+    #print(","+str(actual_1))
     
     #print(str(actual_0)+","+str(actual_1)+","+str(actual_2)+","+str(actual_3)+","+str(actual_4)+","+str(actual_5)+","+str(actual_6)+","+str(actual_7))
     #a=[chip.read(0),chip.read(1),chip.read(2),chip.read(3),chip.read(4),chip.read(5),chip.read(6),chip.read(7)]
-    a=[chip.read(7),chip.read(6),chip.read(5),chip.read(4),chip.read(0),chip.read(1),chip.read(2),chip.read(3)]
+ #   a=[chip.read(7),chip.read(6),chip.read(5),chip.read(4),chip.read(0),chip.read(1),chip.read(2),chip.read(3)]
     #d=(l[0]-a[0])**2+(l[1]-a[1])**2+(l[2]-a[2])**2+(l[3]-a[3])**2+(l[4]-a[4])**2+(l[5]-a[5])**2+(l[6]-a[6])**2+(l[7]-a[7])**2
-    d=max(abs(l[0]-a[0]),abs(l[1]-a[1]),abs(l[2]-a[2]),abs(l[3]-a[3]),abs(l[4]-a[4]),abs(l[5]-a[5]),abs(l[6]-a[6]),abs(l[7]-a[7]))
-    #print(d)
-    if (d>20):
-        l=a
+   # d=max(abs(l[0]-a[0]),abs(l[1]-a[1]),abs(l[2]-a[2]),abs(l[3]-a[3]),abs(l[4]-a[4]),abs(l[5]-a[5]),abs(l[6]-a[6]),abs(l[7]-a[7]))
+  #  if (d>20):
+    #    l=a
         #print(d)
-        m=[l[0]//16,l[1]//16,l[2]//16,l[3]//16,l[4]//16,l[5]//16,l[6]//16,l[7]//16]
-        print(m)
-        pwm1.set(int((1<<16)*(m[0])/64))
-        if (m[0]>=32 and led.value()==0):
-            led.on()
-        if (m[0]<32 and led.value()==1):
-            led.off()
-    sleep(0.05)
+     #   m=[l[0]//16,l[1]//16,l[2]//16,l[3]//16,l[4]//16,l[5]//16,l[6]//16,l[7]//16]
+      #  print(m)
+       # if (m[0]>=32 and led.value()==0):
+          #  led.on()
+       # if (m[0]<32 and led.value()==1):
+        #    led.off()
+   # sleep(0.1)
