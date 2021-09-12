e6198de8754601e57e5d3ed88403a3e508788da6
n19k6 <46622208+n19k6@users.noreply.github.com>
2021-09-12 16:05:15 +0200

    replaced spaces with tabs

--- v9_python_wrapup/v9_1/string_helper.py
+++ v9_python_wrapup/v9_1/string_helper.py
@@ -1,3 +1,3 @@
 def add_two_strings(first_string, second_string):
-    """Add two strings"""
-    return first_string + " " + second_string
+		"""Add two strings"""
+		return first_string + " " + second_string
